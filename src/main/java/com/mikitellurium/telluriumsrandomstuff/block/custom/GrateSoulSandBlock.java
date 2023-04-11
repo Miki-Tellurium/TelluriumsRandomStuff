3e213e92a613e61249ab03bfb9ac2214bbf82257
Miki-Tellurium <michelegamer14@gmail.com>
2023-04-11 10:17:38 +0200

    Added soul lava particles to grate soul sand

--- src/main/java/com/mikitellurium/telluriumsrandomstuff/block/custom/GrateSoulSandBlock.java
+++ src/main/java/com/mikitellurium/telluriumsrandomstuff/block/custom/GrateSoulSandBlock.java
@@ -18,6 +18,7 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
 import net.minecraft.world.phys.shapes.VoxelShape;
@@ -37,6 +38,15 @@ public class GrateSoulSandBlock extends SoulSandBlock {
 
     @Override
     public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource random) {
+        if (level.getFluidState(pos.above()).is(Fluids.LAVA)) {
+            if (random.nextInt(12) == 0) {
+                double d0 = pos.getX() + random.nextDouble();
+                double d1 = pos.getY() - 0.05d;
+                double d2 = pos.getZ() + random.nextDouble();
+                level.addParticle(ModParticles.SOUL_LAVA_HANG.get(), d0, d1, d2, 0.0D, 0.0D, 0.0D);
+            }
+        }
+
         if (level.isRaining()) {
             LevelUtils.handleRainParticles(level, pos, state, random);
         }
