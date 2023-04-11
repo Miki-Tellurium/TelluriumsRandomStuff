4fbbe5169340599cecc938f6654202880076c1d8
Miki-Tellurium <michelegamer14@gmail.com>
2023-04-11 18:01:06 +0200

    Added valid spawn types

--- src/main/java/com/mikitellurium/telluriumsrandomstuff/block/custom/SoulMagmaBlock.java
+++ src/main/java/com/mikitellurium/telluriumsrandomstuff/block/custom/SoulMagmaBlock.java
@@ -5,6 +5,9 @@ import com.mikitellurium.telluriumsrandomstuff.util.ParticleUtils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.animal.horse.SkeletonHorse;
+import net.minecraft.world.entity.monster.Skeleton;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.MagmaBlock;
@@ -17,6 +20,14 @@ public class SoulMagmaBlock extends MagmaBlock {
                 .isValidSpawn((blockState, blockGetter, blockPos, entityType) -> LevelUtils.isSoulMagmaBlockValidSpawn(entityType))
                 .lightLevel((blockState) -> 2)
                 .emissiveRendering((blockState, blockGetter, blockPos) -> true));
+    }
+
+    @Override
+    public void stepOn(Level pLevel, BlockPos pPos, BlockState pState, Entity pEntity) {
+        // Wither skeleton already immune to fire damage
+        if (!(pEntity instanceof Skeleton || pEntity instanceof SkeletonHorse)) {
+            super.stepOn(pLevel, pPos, pState, pEntity);
+        }
     }
 
     @Override
