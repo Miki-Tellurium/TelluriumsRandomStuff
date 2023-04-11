0559e336ffcde71fa41b5d26a45b38f3befead89
Miki-Tellurium <michelegamer14@gmail.com>
2023-04-11 18:43:42 +0200

    Added soul magma block recipe

--- src/main/java/com/mikitellurium/telluriumsrandomstuff/item/ModItems.java
+++ src/main/java/com/mikitellurium/telluriumsrandomstuff/item/ModItems.java
@@ -4,6 +4,7 @@ import com.mikitellurium.telluriumsrandomstuff.TelluriumsRandomStuffMod;
 import com.mikitellurium.telluriumsrandomstuff.fluid.ModFluids;
 import net.minecraft.world.item.BucketItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -15,7 +16,7 @@ public class ModItems {
     public static DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TelluriumsRandomStuffMod.MOD_ID);
 
     public static final RegistryObject<Item> SOUL_LAVA_BUCKET = ITEMS.register("soul_lava_bucket",
-            () -> new BucketItem(ModFluids.SOUL_LAVA_SOURCE, new Item.Properties().stacksTo(1)));
+            () -> new BucketItem(ModFluids.SOUL_LAVA_SOURCE, new Item.Properties().craftRemainder(Items.BUCKET).stacksTo(1)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
