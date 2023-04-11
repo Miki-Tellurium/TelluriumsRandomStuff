f3d0d4bb64bb8b40544b8288dccf51f8259600ce
Miki-Tellurium <michelegamer14@gmail.com>
2023-04-11 12:27:21 +0200

    Added lava to soul lava conversion mechanic

--- src/main/java/com/mikitellurium/telluriumsrandomstuff/mixin/LiquidBlockMixin.java
+++ src/main/java/com/mikitellurium/telluriumsrandomstuff/mixin/LiquidBlockMixin.java
@@ -1,17 +1,20 @@
 package com.mikitellurium.telluriumsrandomstuff.mixin;
 
-import com.mikitellurium.telluriumsrandomstuff.particle.ModParticles;
+import com.mikitellurium.telluriumsrandomstuff.block.ModBlocks;
+import com.mikitellurium.telluriumsrandomstuff.util.LevelUtils;
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.tags.FluidTags;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.LiquidBlock;
+import net.minecraft.world.level.block.PointedDripstoneBlock;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.material.FlowingFluid;
+import net.minecraft.world.level.material.Fluids;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -19,15 +22,21 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(LiquidBlock.class)
 public abstract class LiquidBlockMixin {
 
-    @Shadow public abstract FlowingFluid getFluid();
-
     @Inject(method = "randomTick", at = @At(value = "TAIL"))
     private void randomConvertLavaToSoulLava(BlockState state, ServerLevel level, BlockPos pos,
                                              RandomSource random, CallbackInfo ci) {
-        if (this.getFluid().is(FluidTags.LAVA)) {
-            if (level.getBlockState(pos.below()).is(Blocks.SOUL_SAND)) {
-                //System.out.println("SOUL_SAND");
+        if (level.getFluidState(pos).is(Fluids.LAVA) && (level.getBlockState(pos.below()).is(Blocks.SOUL_SAND) ||
+                level.getBlockState(pos.below()).is(ModBlocks.GRATE_SOUL_SAND.get()))) {
+
+            if (random.nextFloat() < 0.00420f) {
+                BlockPos blockPos1 = LevelUtils.findFillableCauldronBelow(level, pos.below()); // Find empty cauldron under soul sand
+                if (blockPos1 != null) {
+                    level.setBlockAndUpdate(pos, Blocks.AIR.defaultBlockState());
+                    level.setBlockAndUpdate(blockPos1, ModBlocks.SOUL_LAVA_CAULDRON_BLOCK.get().defaultBlockState());
+                    level.playSound(null, blockPos1, SoundEvents.SOUL_ESCAPE, SoundSource.BLOCKS, 0.8f, 1.0f);
+                }
             }
+
         }
     }
 
---
+++ src/main/java/com/mikitellurium/telluriumsrandomstuff/util/LevelUtils.java
@@ -0,0 +1,69 @@
+package com.mikitellurium.telluriumsrandomstuff.util;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.CauldronBlock;
+import net.minecraft.world.level.block.PointedDripstoneBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.BooleanOp;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+import javax.annotation.Nullable;
+import java.util.Optional;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+
+public class LevelUtils {
+
+    private static final VoxelShape REQUIRED_SPACE_TO_DRIP_THROUGH_NON_SOLID_BLOCK =
+            Block.box(6.0D, 0.0D, 6.0D, 10.0D, 16.0D, 10.0D);
+
+    @Nullable
+    public static BlockPos findFillableCauldronBelow(Level pLevel, BlockPos pPos) {
+        Predicate<BlockState> predicate = (blockState) -> blockState.getBlock() instanceof CauldronBlock;
+        BiPredicate<BlockPos, BlockState> bipredicate = (blockPos, blockState) -> canDripThrough(pLevel, blockPos, blockState);
+        return findBlockVertical(pLevel, pPos, Direction.DOWN.getAxisDirection(), bipredicate, predicate, 11).orElse(null);
+    }
+
+    private static boolean canDripThrough(BlockGetter pLevel, BlockPos pPos, BlockState pState) {
+        if (pState.isAir()) {
+            return true;
+        } else if (pState.isSolidRender(pLevel, pPos)) {
+            return false;
+        } else if (!pState.getFluidState().isEmpty()) {
+            return false;
+        } else {
+            VoxelShape voxelshape = pState.getCollisionShape(pLevel, pPos);
+            return !Shapes.joinIsNotEmpty(REQUIRED_SPACE_TO_DRIP_THROUGH_NON_SOLID_BLOCK, voxelshape, BooleanOp.AND);
+        }
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    private static Optional<BlockPos> findBlockVertical(LevelAccessor pLevel, BlockPos pPos, Direction.AxisDirection pAxis,
+                                                        BiPredicate<BlockPos, BlockState> pPositionalStatePredicate,
+                                                        Predicate<BlockState> pStatePredicate, int pMaxIterations) {
+        Direction direction = Direction.get(pAxis, Direction.Axis.Y);
+        BlockPos.MutableBlockPos blockpos$mutableblockpos = pPos.mutable();
+
+        for(int i = 1; i < pMaxIterations; ++i) {
+            blockpos$mutableblockpos.move(direction);
+            BlockState blockstate = pLevel.getBlockState(blockpos$mutableblockpos);
+            if (pStatePredicate.test(blockstate)) {
+                return Optional.of(blockpos$mutableblockpos.immutable());
+            }
+
+            if (pLevel.isOutsideBuildHeight(blockpos$mutableblockpos.getY()) ||
+                    !pPositionalStatePredicate.test(blockpos$mutableblockpos, blockstate)) {
+                return Optional.empty();
+            }
+        }
+
+        return Optional.empty();
+    }
+
+}
