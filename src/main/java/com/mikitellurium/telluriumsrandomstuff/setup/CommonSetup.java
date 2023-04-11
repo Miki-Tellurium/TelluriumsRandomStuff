960755553f66e94911e93eea9191b58578349cd2
Miki-Tellurium <michelegamer14@gmail.com>
2023-04-11 18:32:24 +0200

    Repurposed class

---
+++ src/main/java/com/mikitellurium/telluriumsrandomstuff/setup/CommonSetup.java
@@ -0,0 +1,9 @@
+package com.mikitellurium.telluriumsrandomstuff.setup;
+
+import com.mikitellurium.telluriumsrandomstuff.TelluriumsRandomStuffMod;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = TelluriumsRandomStuffMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class CommonSetup {
+
+}
