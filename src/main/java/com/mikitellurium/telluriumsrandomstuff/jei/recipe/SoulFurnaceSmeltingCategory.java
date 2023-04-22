b4f080427a15e5c9912cd4d8cacd4a4f6fd05c45
Miki-Tellurium <michelegamer14@gmail.com>
2023-04-22 18:41:33 +0200

    Improvement to jei soul furnace gui

--- src/main/java/com/mikitellurium/telluriumsrandomstuff/jei/recipe/SoulFurnaceSmeltingCategory.java
+++ src/main/java/com/mikitellurium/telluriumsrandomstuff/jei/recipe/SoulFurnaceSmeltingCategory.java
@@ -1,13 +1,21 @@
 package com.mikitellurium.telluriumsrandomstuff.jei.recipe;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.mikitellurium.telluriumsrandomstuff.TelluriumsRandomStuffMod;
 import com.mikitellurium.telluriumsrandomstuff.block.ModBlocks;
 import com.mikitellurium.telluriumsrandomstuff.fluid.ModFluids;
 import com.mikitellurium.telluriumsrandomstuff.item.ModItems;
 import com.mikitellurium.telluriumsrandomstuff.jei.JeiIntegration;
+import com.mojang.blaze3d.vertex.PoseStack;
 import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.forge.ForgeTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.drawable.IDrawableAnimated;
+import mezz.jei.api.gui.drawable.IDrawableStatic;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
@@ -17,20 +25,50 @@ import net.minecraft.core.RegistryAccess;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.fluids.FluidStack;
+
+import java.util.List;
 
 public class SoulFurnaceSmeltingCategory implements IRecipeCategory<SoulFurnaceRecipe> {
 
     public final static ResourceLocation UID =
             new ResourceLocation(TelluriumsRandomStuffMod.MOD_ID, "soul_furnace_smelting");
-    public final static ResourceLocation TEXTURE =
-            new ResourceLocation(TelluriumsRandomStuffMod.MOD_ID, "textures/gui/soul_furnace_gui.png");
+    public final static ResourceLocation GUI_TEXTURE =
+            new ResourceLocation(TelluriumsRandomStuffMod.MOD_ID, "textures/jei/jei_soul_furnace_gui.png");
 
     private final IDrawable background;
     private final IDrawable icon;
+    private final IDrawable tankGlass;
+    private final LoadingCache<Integer, IDrawableAnimated> cachedArrows;
+    private final int smeltingTime = 100;
+    protected final IDrawableStatic staticFlame;
+    protected final IDrawableAnimated animatedFlame;
 
-    public SoulFurnaceSmeltingCategory(IGuiHelper helper) {
-        this.background = helper.createDrawable(TEXTURE, 0, 0, 176, 81);
-        this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(ModBlocks.SOUL_FURNACE.get()));
+    public SoulFurnaceSmeltingCategory(IGuiHelper guiHelper) {
+        this.background = guiHelper.createDrawable(GUI_TEXTURE, 0, 0, 120, 72);
+        this.icon = guiHelper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(ModBlocks.SOUL_FURNACE.get()));
+        this.cachedArrows = CacheBuilder.newBuilder()
+                .maximumSize(25)
+                .build(new CacheLoader<>() {
+                    @Override
+                    public IDrawableAnimated load(Integer cookTime) {
+                        return guiHelper.drawableBuilder(GUI_TEXTURE, 176, 14, 24, 17)
+                                .buildAnimated(cookTime, IDrawableAnimated.StartDirection.LEFT, false);
+                    }
+                });
+        this.staticFlame = guiHelper.createDrawable(GUI_TEXTURE, 176, 0, 14, 14);
+        this.animatedFlame = guiHelper.createAnimatedDrawable(staticFlame, 300, IDrawableAnimated.StartDirection.TOP, true);
+        this.tankGlass = guiHelper.createDrawable(GUI_TEXTURE, 176, 31, 16, 48);
+    }
+
+    protected IDrawableAnimated getArrow() {
+        return this.cachedArrows.getUnchecked(smeltingTime);
+    }
+
+    @Override
+    public void draw(SoulFurnaceRecipe recipe, IRecipeSlotsView recipeSlotsView, PoseStack poseStack, double mouseX, double mouseY) {
+        animatedFlame.draw(poseStack, 37, 43);
+        getArrow().draw(poseStack, 58, 24);
     }
 
     @Override
@@ -55,10 +93,13 @@ public class SoulFurnaceSmeltingCategory implements IRecipeCategory<SoulFurnaceR
 
     @Override
     public void setRecipe(IRecipeLayoutBuilder builder, SoulFurnaceRecipe recipe, IFocusGroup focuses) {
-        builder.addSlot(RecipeIngredientRole.INPUT, 56, 30).addIngredients(recipe.getIngredients().get(0));
-        builder.addSlot(RecipeIngredientRole.OUTPUT, 116, 30).addItemStack(recipe.getResultItem(RegistryAccess.EMPTY));
+        builder.addSlot(RecipeIngredientRole.INPUT, 35, 24).addIngredients(recipe.getIngredients().get(0));
+        builder.addSlot(RecipeIngredientRole.OUTPUT, 95, 24).addItemStack(recipe.getResultItem(RegistryAccess.EMPTY));
+        builder.addSlot(RecipeIngredientRole.RENDER_ONLY, 4, 2)
+                .addIngredients(ForgeTypes.FLUID_STACK, List.of(new FluidStack(ModFluids.SOUL_LAVA_SOURCE.get(), 3000)))
+                .setFluidRenderer(4000, false, 16, 48)
+                .setOverlay(tankGlass, 0, 0);
         builder.addInvisibleIngredients(RecipeIngredientRole.INPUT).addItemStack(ModItems.SOUL_LAVA_BUCKET.get().getDefaultInstance());
-        builder.addInvisibleIngredients(RecipeIngredientRole.INPUT).addFluidStack(ModFluids.SOUL_LAVA_SOURCE.get(), 50);
     }
 
 }
