bb3f385d25fb9146e6111b7c1d7c2a484d6f49b7
Miki_Tellurium <michelegamer14@gmail.com>
2023-04-27 20:12:26 +0200

    Added opalium world gen

--- src/main/java/com/mikitellurium/telluriumsrandomstuff/worldgen/ModConfiguredFeatures.java
+++ src/main/java/com/mikitellurium/telluriumsrandomstuff/worldgen/ModConfiguredFeatures.java
@@ -1,40 +1,45 @@
 package com.mikitellurium.telluriumsrandomstuff.worldgen;
 
 
-import com.google.common.base.Suppliers;
 import com.mikitellurium.telluriumsrandomstuff.TelluriumsRandomStuffMod;
 import com.mikitellurium.telluriumsrandomstuff.block.ModBlocks;
-import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
-import net.minecraft.data.worldgen.features.FeatureUtils;
-import net.minecraft.data.worldgen.features.OreFeatures;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
-import net.minecraft.world.level.levelgen.feature.OreFeature;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraft.world.level.levelgen.structure.templatesystem.BlockMatchTest;
 import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
 import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest;
 
 import java.util.List;
-import java.util.function.Supplier;
 
 public class ModConfiguredFeatures {
 
-    public static final ResourceKey<ConfiguredFeature<?, ?>> OVERWORLD_OPAL_CLUSTER_KEY =
+    public static final ResourceKey<ConfiguredFeature<?, ?>> OVERWORLD_OPAL_CLUSTER =
             registerKey("opal_cluster");
+
+    public static final ResourceKey<ConfiguredFeature<?, ?>> OVERWORLD_OPALIUM_ORE_SMALL =
+            registerKey("opalium_ore_small");
+    public static final ResourceKey<ConfiguredFeature<?, ?>> OVERWORLD_OPALIUM_ORE_LARGE =
+            registerKey("opalium_ore_large");
 
     public static void bootstrap(BootstapContext<ConfiguredFeature<?, ?>> context) {
         RuleTest stoneReplaceables = new TagMatchTest(BlockTags.BASE_STONE_OVERWORLD);
+        RuleTest opalClusterReplaceable = new BlockMatchTest(ModBlocks.OPAL.get());
 
         List<OreConfiguration.TargetBlockState> overworldOpalCluster = List.of(
                 OreConfiguration.target(stoneReplaceables, ModBlocks.OPAL.get().defaultBlockState()));
+        List<OreConfiguration.TargetBlockState> overworldOpaliumOre = List.of(
+                OreConfiguration.target(opalClusterReplaceable, ModBlocks.OPALIUM_ORE.get().defaultBlockState()));
 
-        register(context, OVERWORLD_OPAL_CLUSTER_KEY, Feature.ORE, new OreConfiguration(overworldOpalCluster,64));
+        register(context, OVERWORLD_OPAL_CLUSTER, Feature.ORE, new OreConfiguration(overworldOpalCluster,64));
+        register(context, OVERWORLD_OPALIUM_ORE_SMALL, Feature.ORE, new OreConfiguration(overworldOpaliumOre,4));
+        register(context, OVERWORLD_OPALIUM_ORE_LARGE, Feature.ORE, new OreConfiguration(overworldOpaliumOre,12));
     }
 
 
--- src/main/java/com/mikitellurium/telluriumsrandomstuff/worldgen/ModPlacedFeatures.java
+++ src/main/java/com/mikitellurium/telluriumsrandomstuff/worldgen/ModPlacedFeatures.java
@@ -1,13 +1,11 @@
 package com.mikitellurium.telluriumsrandomstuff.worldgen;
 
 import com.mikitellurium.telluriumsrandomstuff.TelluriumsRandomStuffMod;
-import com.mikitellurium.telluriumsrandomstuff.block.ModBlocks;
 import com.mikitellurium.telluriumsrandomstuff.util.OrePlacementUtils;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderGetter;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
-import net.minecraft.data.worldgen.placement.OrePlacements;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
@@ -18,14 +16,24 @@ import java.util.List;
 
 public class ModPlacedFeatures {
 
-    public static final ResourceKey<PlacedFeature> OPAL_CLUSTER_PLACED_KEY = createKey("opal_cluster_placed");
+    public static final ResourceKey<PlacedFeature> OPAL_CLUSTER_PLACED = createKey("opal_cluster_placed");
+
+    public static final ResourceKey<PlacedFeature> OPALIUM_ORE_SMALL_PLACED = createKey("opalium_ore_small_placed");
+    public static final ResourceKey<PlacedFeature> OPALIUM_ORE_LARGE_PLACED = createKey("opalium_ore_large_placed");
 
     public static void bootstrap(BootstapContext<PlacedFeature> context) {
         HolderGetter<ConfiguredFeature<?, ?>> configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
 
-        register(context, OPAL_CLUSTER_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.OVERWORLD_OPAL_CLUSTER_KEY),
-                OrePlacementUtils.rareOrePlacement(10,
-                        HeightRangePlacement.uniform(VerticalAnchor.absolute(-60), VerticalAnchor.absolute(128))));
+        register(context, OPAL_CLUSTER_PLACED, configuredFeatures.getOrThrow(ModConfiguredFeatures.OVERWORLD_OPAL_CLUSTER),
+                OrePlacementUtils.rareOrePlacement(10, HeightRangePlacement.uniform(
+                        VerticalAnchor.absolute(-60), VerticalAnchor.absolute(128))));
+
+        register(context, OPALIUM_ORE_SMALL_PLACED, configuredFeatures.getOrThrow(ModConfiguredFeatures.OVERWORLD_OPALIUM_ORE_SMALL),
+                OrePlacementUtils.commonOrePlacement(15, HeightRangePlacement.uniform(
+                        VerticalAnchor.absolute(-60), VerticalAnchor.absolute(128))));
+        register(context, OPALIUM_ORE_LARGE_PLACED, configuredFeatures.getOrThrow(ModConfiguredFeatures.OVERWORLD_OPALIUM_ORE_LARGE),
+                OrePlacementUtils.commonOrePlacement(15, HeightRangePlacement.uniform(
+                        VerticalAnchor.absolute(-60), VerticalAnchor.absolute(128))));
     }
 
     private static ResourceKey<PlacedFeature> createKey(String name) {
