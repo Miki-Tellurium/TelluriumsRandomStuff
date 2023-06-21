2b9e4d46e5f46cf7d6a488d1507f6eeadefbc751
Miki_Tellurium <michelegamer14@gmail.com>
2023-06-21 15:30:59 +0200

    Added compatible enchantments for lava googles

--- src/main/java/com/mikitellurium/telluriumsrandomstuff/common/content/item/LavaGooglesItem.java
+++ src/main/java/com/mikitellurium/telluriumsrandomstuff/common/content/item/LavaGooglesItem.java
@@ -3,6 +3,9 @@ package com.mikitellurium.telluriumsrandomstuff.common.content.item;
 import com.mikitellurium.telluriumsrandomstuff.registry.ModArmorMaterials;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
 
 public class LavaGooglesItem extends ArmorItem {
 
@@ -10,6 +13,14 @@ public class LavaGooglesItem extends ArmorItem {
         super(ModArmorMaterials.LAVA_GOOGLES, Type.HELMET, new Item.Properties()
                 .defaultDurability(64)
                 .fireResistant());
+    }
+
+    @Override
+    public boolean canApplyAtEnchantingTable(ItemStack stack, Enchantment enchantment) {
+        return enchantment.equals(Enchantments.VANISHING_CURSE) ||
+                enchantment.equals(Enchantments.UNBREAKING) ||
+                enchantment.equals(Enchantments.MENDING) ||
+                enchantment.equals(Enchantments.FIRE_PROTECTION);
     }
 
 }
