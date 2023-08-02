4881ab19c80fef5d92e0ad2f993c827fc9c56d3e
Miki_Tellurium <michelegamer14@gmail.com>
2023-08-02 10:01:49 +0200

    Adjusted soul lava cauldron interaction with entities

--- src/main/java/com/mikitellurium/telluriumsrandomstuff/common/content/block/SoulLavaCauldronBlock.java
+++ src/main/java/com/mikitellurium/telluriumsrandomstuff/common/content/block/SoulLavaCauldronBlock.java
@@ -1,8 +1,11 @@
 package com.mikitellurium.telluriumsrandomstuff.common.content.block;
 
 import com.mikitellurium.telluriumsrandomstuff.common.content.block.interaction.ModCauldronInteractions;
+import com.mikitellurium.telluriumsrandomstuff.common.content.fluid.SoulLavaFluid;
+import com.mikitellurium.telluriumsrandomstuff.registry.ModTags;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -20,29 +23,35 @@ public class SoulLavaCauldronBlock extends AbstractCauldronBlock {
         super(BlockBehaviour.Properties.copy(Blocks.LAVA_CAULDRON), ModCauldronInteractions.SOUL_LAVA);
     }
 
-    protected double getContentHeight(BlockState state) {
+    protected double getContentHeight(BlockState blockState) {
         return 0.9375D;
     }
 
     @Override
-    public boolean isFull(BlockState pState) {
+    public boolean isFull(BlockState blockState) {
         return true;
     }
 
     @Override
-    public void entityInside(BlockState pState, Level pLevel, BlockPos pPos, Entity pEntity) {
-        if (this.isEntityInsideContent(pState, pPos, pEntity)) {
-            pEntity.lavaHurt();
+    public void entityInside(BlockState blockState, Level level, BlockPos pos, Entity entity) {
+        if (this.isEntityInsideContent(blockState, pos, entity) &&
+                !entity.getType().is(ModTags.EntityTypes.SOUL_LAVA_IMMUNE)) {
+            if (entity instanceof LivingEntity livingEntity) {
+                SoulLavaFluid.soulLavaHurt(livingEntity);
+            } else {
+                entity.lavaHurt();
+            }
         }
     }
 
     @Override
-    public int getAnalogOutputSignal(BlockState pState, Level pLevel, BlockPos pPos) {
+    public int getAnalogOutputSignal(BlockState blockState, Level level, BlockPos pos) {
         return 5;
     }
 
     @Override
-    public ItemStack getCloneItemStack(BlockState state, HitResult target, BlockGetter level, BlockPos pos, Player player) {
+    public ItemStack getCloneItemStack(BlockState blockState, HitResult target, BlockGetter level, BlockPos pos,
+                                       Player player) {
         return Items.CAULDRON.getDefaultInstance();
     }
 
