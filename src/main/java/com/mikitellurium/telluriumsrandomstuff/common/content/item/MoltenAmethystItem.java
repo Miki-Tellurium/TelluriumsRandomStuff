1c57d71a7c0dda9310acbfccdaa9a9d35de40433
Miki_Tellurium <michelegamer14@gmail.com>
2023-09-17 11:53:18 +0200

    Code optimization

--- src/main/java/com/mikitellurium/telluriumsrandomstuff/common/content/item/MoltenAmethystItem.java
+++ src/main/java/com/mikitellurium/telluriumsrandomstuff/common/content/item/MoltenAmethystItem.java
@@ -1,6 +1,7 @@
 package com.mikitellurium.telluriumsrandomstuff.common.content.item;
 
 import com.mikitellurium.telluriumsrandomstuff.registry.ModItems;
+import com.mikitellurium.telluriumsrandomstuff.util.LevelUtils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.item.ItemEntity;
@@ -18,7 +19,7 @@ public class MoltenAmethystItem extends Item {
 
     @Override
     public boolean onEntityItemUpdate(ItemStack stack, ItemEntity entity) {
-        if (!entity.isInWaterOrBubble() && !isInsideWaterCauldron(entity.level(), entity)) {
+        if (!entity.isInWaterOrBubble() && !LevelUtils.isInsideWaterCauldron(entity.level(), entity)) {
             return false;
         } else {
             Level level = entity.level();
@@ -30,11 +31,6 @@ public class MoltenAmethystItem extends Item {
             lens.getDeltaMovement().add(0.0D, 0.02D, 0.0D);
             return true;
         }
-    }
-
-    public boolean isInsideWaterCauldron(Level level, ItemEntity entity) {
-        BlockPos blockPos = entity.blockPosition();
-        return level.getBlockState(blockPos).is(Blocks.WATER_CAULDRON);
     }
 
 }
