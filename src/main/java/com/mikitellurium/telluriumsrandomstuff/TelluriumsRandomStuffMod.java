02b435c750611350052479db9d8a84d3d81e0623
Miki_Tellurium <michelegamer14@gmail.com>
2023-09-19 20:08:59 +0200

    Moved creative tab content registration

--- src/main/java/com/mikitellurium/telluriumsrandomstuff/TelluriumsRandomStuffMod.java
+++ src/main/java/com/mikitellurium/telluriumsrandomstuff/TelluriumsRandomStuffMod.java
@@ -33,7 +33,6 @@ public class TelluriumsRandomStuffMod {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         modEventBus.addListener(this::commonSetup);
-        modEventBus.addListener(this::addCreativeTab);
         MinecraftForge.EVENT_BUS.register(this);
 
         Registries.init(modEventBus);
@@ -45,80 +44,6 @@ public class TelluriumsRandomStuffMod {
         ModDispenserBehaviours.register();
         ((FlowerPotBlock)Blocks.FLOWER_POT).addPlant(ModBlocks.BRIGHT_TORCHFLOWER.getId(), ModBlocks.POTTED_BRIGHT_TORCHFLOWER);
         ((FlowerPotBlock)Blocks.FLOWER_POT).addPlant(ModBlocks.SOUL_TORCHFLOWER.getId(), ModBlocks.POTTED_SOUL_TORCHFLOWER);
-    }
-
-    private void addCreativeTab(BuildCreativeModeTabContentsEvent event) {
-        ItemStack soulHarvestingI = new ItemStack(Items.ENCHANTED_BOOK);
-        ItemStack soulHarvestingII = soulHarvestingI.copy();
-        ItemStack soulHarvestingIII = soulHarvestingI.copy();
-        EnchantedBookItem.addEnchantment(soulHarvestingI, new EnchantmentInstance(ModEnchantments.SOUL_HARVESTING.get(), 1));
-        EnchantedBookItem.addEnchantment(soulHarvestingII, new EnchantmentInstance(ModEnchantments.SOUL_HARVESTING.get(), 2));
-        EnchantedBookItem.addEnchantment(soulHarvestingIII, new EnchantmentInstance(ModEnchantments.SOUL_HARVESTING.get(), 3));
-
-        if (event.getTab() == ModCreativeTab.TAB_TELLURIUMSRANDOMSTUFF.get()) {
-            event.accept(ModBlocks.GRATE_MAGMA_BLOCK);
-            event.accept(ModBlocks.GRATE_SOUL_SAND);
-            event.accept(ModBlocks.HYDRODYNAMIC_RAIL);
-            event.accept(ModItems.SOUL_LAVA_BUCKET);
-            event.accept(ModBlocks.SOUL_MAGMA_BLOCK);
-            event.accept(ModBlocks.GRATE_SOUL_MAGMA_BLOCK);
-            event.accept(ModBlocks.SOUL_FURNACE);
-            event.accept(ModBlocks.SOUL_MAGMA_BRICKS);
-            event.accept(ModBlocks.SOUL_MAGMA_BRICK_SLAB);
-            event.accept(ModBlocks.SOUL_OBSIDIAN);
-            event.accept(ModBlocks.SOUL_ANCHOR);
-            event.accept(ModItems.MYSTIC_POTATO);
-            event.accept(soulHarvestingI);
-            event.accept(soulHarvestingII);
-            event.accept(soulHarvestingIII);
-            event.accept(ModBlocks.OPAL);
-            event.accept(ModBlocks.OPAL_COBBLESTONE);
-            event.accept(ModBlocks.OPAL_BRICKS);
-            event.accept(ModBlocks.CUT_OPAL_BRICKS);
-            event.accept(ModBlocks.CRACKED_OPAL_BRICKS);
-            event.accept(ModBlocks.CRACKED_CUT_OPAL_BRICKS);
-            event.accept(ModBlocks.CHISELED_OPAL_BRICKS);
-            event.accept(ModBlocks.OPAL_SLAB);
-            event.accept(ModBlocks.OPAL_COBBLESTONE_SLAB);
-            event.accept(ModBlocks.OPAL_BRICK_SLAB);
-            event.accept(ModBlocks.CUT_OPAL_BRICK_SLAB);
-            event.accept(ModBlocks.CRACKED_OPAL_BRICK_SLAB);
-            event.accept(ModBlocks.CRACKED_CUT_OPAL_BRICK_SLAB);
-            event.accept(ModBlocks.OPAL_STAIRS);
-            event.accept(ModBlocks.OPAL_COBBLESTONE_STAIRS);
-            event.accept(ModBlocks.OPAL_BRICK_STAIRS);
-            event.accept(ModBlocks.CUT_OPAL_BRICK_STAIRS);
-            event.accept(ModBlocks.OPAL_COBBLESTONE_WALL);
-            event.accept(ModBlocks.OPAL_BRICK_WALL);
-            event.accept(ModBlocks.CUT_OPAL_BRICK_WALL);
-            event.accept(ModBlocks.OPAL_PRESSURE_PLATE);
-            event.accept(ModBlocks.OPAL_BUTTON);
-            event.accept(ModBlocks.OPAL_CRYSTAL_ORE);
-            event.accept(ModBlocks.OPAL_CRYSTAL_BLOCK);
-            event.accept(ModBlocks.RAW_OPAL_CRYSTAL_BLOCK);
-            event.accept(ModItems.RAW_OPAL_CRYSTAL);
-            event.accept(ModItems.OPAL_CRYSTAL);
-            event.accept(ModItems.OPAL_CRYSTAL_SHOVEL);
-            event.accept(ModItems.OPAL_CRYSTAL_PICKAXE);
-            event.accept(ModItems.OPAL_CRYSTAL_AXE);
-            event.accept(ModItems.OPAL_CRYSTAL_HOE);
-            event.accept(ModItems.OPAL_CRYSTAL_SWORD);
-            event.accept(ModItems.FILTER);
-            event.accept(ModBlocks.EXTRACTOR);
-            event.accept(ModBlocks.BRIGHT_TORCHFLOWER);
-            event.accept(ModItems.BRIGHT_TORCHFLOWER_SEEDS);
-            event.accept(ModBlocks.SOUL_TORCHFLOWER);
-            event.accept(ModItems.SOUL_TORCHFLOWER_SEEDS);
-            event.accept(ModBlocks.BLUE_GLOWSTONE);
-            event.accept(ModItems.BLUE_GLOWSTONE_DUST);
-            event.accept(ModBlocks.BLUE_REDSTONE_LAMP);
-            event.accept(ModItems.MOLTEN_AMETHYST);
-            event.accept(ModItems.AMETHYST_LENS);
-            event.accept(ModItems.LAVA_GOOGLES);
-            event.accept(ModBlocks.AWAKENED_SCULK_SHRIEKER);
-            event.accept(ModBlocks.SOUL_JACK_O_LANTERN);
-            event.accept(ModBlocks.STONE_ITEM_PEDESTAL);
-        }
     }
 
     @SubscribeEvent
