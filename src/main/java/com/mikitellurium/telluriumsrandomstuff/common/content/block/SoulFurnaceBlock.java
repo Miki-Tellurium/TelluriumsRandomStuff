3a8090b0e0209a66f236a248c0a3c43d7d76c619
Miki_Tellurium <michelegamer14@gmail.com>
2023-09-19 14:53:02 +0200

    Minor changes

--- src/main/java/com/mikitellurium/telluriumsrandomstuff/common/content/block/SoulFurnaceBlock.java
+++ src/main/java/com/mikitellurium/telluriumsrandomstuff/common/content/block/SoulFurnaceBlock.java
@@ -36,23 +36,24 @@ public class SoulFurnaceBlock extends AbstractFurnaceBlock {
     }
 
     @Override
-    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
-        if (!pLevel.isClientSide) {
-            BlockEntity blockEntity = pLevel.getBlockEntity(pPos);
-            if (blockEntity instanceof SoulFurnaceBlockEntity && pPlayer instanceof ServerPlayer) {
-                this.openContainer(pLevel, pPos, pPlayer);
+    public InteractionResult use(BlockState blockState, Level level, BlockPos pos, Player player, InteractionHand hand,
+                                 BlockHitResult hitResult) {
+        if (!level.isClientSide) {
+            BlockEntity blockEntity = level.getBlockEntity(pos);
+            if (blockEntity instanceof SoulFurnaceBlockEntity && player instanceof ServerPlayer) {
+                this.openContainer(level, pos, player);
             } else {
                 throw new IllegalStateException("Container provider or player is missing");
             }
         }
 
-        return InteractionResult.sidedSuccess(pLevel.isClientSide);
+        return InteractionResult.sidedSuccess(level.isClientSide);
     }
 
     @Override
-    protected void openContainer(Level pLevel, BlockPos pPos, Player pPlayer) {
-        NetworkHooks.openScreen((ServerPlayer)pPlayer, (SoulFurnaceBlockEntity)pLevel.getBlockEntity(pPos), pPos);
-        SoulFurnaceBlockEntity blockEntity = (SoulFurnaceBlockEntity) pLevel.getBlockEntity(pPos);
+    protected void openContainer(Level level, BlockPos pos, Player player) {
+        NetworkHooks.openScreen((ServerPlayer)player, (SoulFurnaceBlockEntity)level.getBlockEntity(pos), pos);
+        SoulFurnaceBlockEntity blockEntity = (SoulFurnaceBlockEntity) level.getBlockEntity(pos);
         // Update the fluid data on the client
         if (blockEntity != null) {
             ModMessages.sendToClients(new FluidSyncS2CPacket(blockEntity.getFluid(), blockEntity.getBlockPos()));
@@ -62,49 +63,49 @@ public class SoulFurnaceBlock extends AbstractFurnaceBlock {
     }
 
     @Override
-    public void animateTick(BlockState pState, Level pLevel, BlockPos pPos, RandomSource pRandom) {
-        if (pState.getValue(LIT)) {
-            double pX = pPos.getX() + 0.5D;
-            double pY = pPos.getY();
-            double pZ = pPos.getZ() + 0.5D;
-            if (pRandom.nextDouble() < 0.1D) {
-                pLevel.playLocalSound(pX, pY, pZ, SoundEvents.FURNACE_FIRE_CRACKLE, SoundSource.BLOCKS, 1.0F, 1.0F, false);
+    public void animateTick(BlockState blockState, Level level, BlockPos pos, RandomSource random) {
+        if (blockState.getValue(LIT)) {
+            double pX = pos.getX() + 0.5D;
+            double pY = pos.getY();
+            double pZ = pos.getZ() + 0.5D;
+            if (random.nextDouble() < 0.1D) {
+                level.playLocalSound(pX, pY, pZ, SoundEvents.FURNACE_FIRE_CRACKLE, SoundSource.BLOCKS, 1.0F, 1.0F, false);
             }
 
-            Direction direction = pState.getValue(FACING);
+            Direction direction = blockState.getValue(FACING);
             Direction.Axis direction$axis = direction.getAxis();
-            double d0 = pRandom.nextDouble() * 0.6D - 0.3D;
+            double d0 = random.nextDouble() * 0.6D - 0.3D;
             double d1 = direction$axis == Direction.Axis.X ? (double)direction.getStepX() * 0.52D : d0;
-            double d2 = pRandom.nextDouble() * 6.0D / 16.0D;
+            double d2 = random.nextDouble() * 6.0D / 16.0D;
             double d3 = direction$axis == Direction.Axis.Z ? (double)direction.getStepZ() * 0.52D : d0;
-            pLevel.addParticle(ParticleTypes.SMOKE, pX + d1, pY + d2, pZ + d3, 0.0D, 0.0D, 0.0D);
-            pLevel.addParticle(ParticleTypes.SOUL_FIRE_FLAME, pX + d1, pY + d2, pZ + d3, 0.0D, 0.0D, 0.0D);
+            level.addParticle(ParticleTypes.SMOKE, pX + d1, pY + d2, pZ + d3, 0.0D, 0.0D, 0.0D);
+            level.addParticle(ParticleTypes.SOUL_FIRE_FLAME, pX + d1, pY + d2, pZ + d3, 0.0D, 0.0D, 0.0D);
         }
     }
 
     @Nullable
     @Override
-    public BlockEntity newBlockEntity(BlockPos pPos, BlockState pState) {
-        return new SoulFurnaceBlockEntity(pPos, pState);
+    public BlockEntity newBlockEntity(BlockPos pos, BlockState blockState) {
+        return new SoulFurnaceBlockEntity(pos, blockState);
     }
 
     @Nullable
     @Override
-    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level pLevel, BlockState pState,
-                                                                  BlockEntityType<T> pBlockEntityType) {
-        return createTickerHelper(pBlockEntityType, ModBlockEntities.SOUL_FURNACE.get(),
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState blockState,
+                                                                  BlockEntityType<T> blockEntityType) {
+        return createTickerHelper(blockEntityType, ModBlockEntities.SOUL_FURNACE.get(),
                 SoulFurnaceBlockEntity::tick);
     }
 
     @Override
-    public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pIsMoving) {
-        if (pState.getBlock() != pNewState.getBlock()) {
-            BlockEntity blockEntity = pLevel.getBlockEntity(pPos);
+    public void onRemove(BlockState blockState, Level level, BlockPos pos, BlockState newState, boolean isMoving) {
+        if (blockState.getBlock() != newState.getBlock()) {
+            BlockEntity blockEntity = level.getBlockEntity(pos);
             if (blockEntity instanceof SoulFurnaceBlockEntity) {
                 ((SoulFurnaceBlockEntity) blockEntity).dropItemsOnBreak();
             }
         }
-        super.onRemove(pState, pLevel, pPos, pNewState, pIsMoving);
+        super.onRemove(blockState, level, pos, newState, isMoving);
     }
 
     public static int getLightLevel(BlockState state) {
