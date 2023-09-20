77a8535fc35ade0c80137b4ea6567089dee0a348
Miki_Tellurium <michelegamer14@gmail.com>
2023-09-20 18:22:04 +0200

    Fixed broken tags

--- src/main/java/com/mikitellurium/telluriumsrandomstuff/registry/ModTags.java
+++ src/main/java/com/mikitellurium/telluriumsrandomstuff/registry/ModTags.java
@@ -18,6 +18,7 @@ public class ModTags {
         public static final TagKey<Block> BUBBLE_COLUMN_DRAG_DOWN = create("bubble_column_drag_down");
         public static final TagKey<Block> BUBBLE_COLUMN_LIFT_UP = create("bubble_column_lift_up");
         public static final TagKey<Block> SOUL_LAVA_CONVERTER = create("soul_lava_converter");
+        public static final TagKey<Block> NEEDS_OPAL_CRYSTAL_TOOL = create("needs_opal_crystal_tools");
 
         public static TagKey<Block> create(String name) {
             return BlockTags.create(new ResourceLocation(TelluriumsRandomStuffMod.MOD_ID, name));
--- src/main/java/com/mikitellurium/telluriumsrandomstuff/registry/ModToolTiers.java
+++ src/main/java/com/mikitellurium/telluriumsrandomstuff/registry/ModToolTiers.java
@@ -2,7 +2,6 @@ package com.mikitellurium.telluriumsrandomstuff.registry;
 
 import com.mikitellurium.telluriumsrandomstuff.TelluriumsRandomStuffMod;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.Tiers;
 import net.minecraft.world.item.crafting.Ingredient;
@@ -15,8 +14,8 @@ public class ModToolTiers {
 
     public static final Tier OPAL_CRYSTAL = TierSortingRegistry.registerTier(
             new ForgeTier(3, 250, 10.0f, 3.0f, 15,
-                    BlockTags.NEEDS_DIAMOND_TOOL, () -> Ingredient.of(ModItems.OPAL_CRYSTAL.get())),
+                    ModTags.Blocks.NEEDS_OPAL_CRYSTAL_TOOL, () -> Ingredient.of(ModItems.OPAL_CRYSTAL.get())),
             new ResourceLocation(TelluriumsRandomStuffMod.MOD_ID, "opal_crystal"),
-            List.of(Tiers.IRON), List.of(Tiers.NETHERITE));
+            List.of(Tiers.DIAMOND), List.of());
 
 }
