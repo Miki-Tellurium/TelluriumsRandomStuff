dfed01591654fbbca686e1ef3898b5a11f7719ca
Miki_Tellurium <michelegamer14@gmail.com>
2023-09-26 10:52:20 +0200

    Added set random color loot function

---
+++ src/main/java/com/mikitellurium/telluriumsrandomstuff/registry/ModLootItemFunctions.java
@@ -0,0 +1,23 @@
+package com.mikitellurium.telluriumsrandomstuff.registry;
+
+import com.mikitellurium.telluriumsrandomstuff.TelluriumsRandomStuffMod;
+import com.mikitellurium.telluriumsrandomstuff.common.content.loot.SetRandomGooglesColorFunction;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.level.storage.loot.functions.*;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModLootItemFunctions {
+
+    public static DeferredRegister<LootItemFunctionType> LOOT_ITEM_FUNCTION_TYPES =
+            DeferredRegister.create(Registries.LOOT_FUNCTION_TYPE, TelluriumsRandomStuffMod.MOD_ID);
+
+    public static RegistryObject<LootItemFunctionType> SET_RANDOM_GOOGLES_COLOR = LOOT_ITEM_FUNCTION_TYPES.register(
+            "set_random_googles_color", () -> new LootItemFunctionType(new SetRandomGooglesColorFunction.Serializer()));
+
+    public static void register(IEventBus eventBus) {
+        LOOT_ITEM_FUNCTION_TYPES.register(eventBus);
+    }
+
+}
