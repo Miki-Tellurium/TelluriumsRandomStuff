e0e19e3dba8cbd12e1b21ff67dd4b94f226dadc7
Miki_Tellurium <michelegamer14@gmail.com>
2023-10-14 15:53:13 +0200

    Updated to use GuiGraphics

--- src/main/java/com/mikitellurium/telluriumsrandomstuff/client/gui/render/GuiFluidRenderer.java
+++ src/main/java/com/mikitellurium/telluriumsrandomstuff/client/gui/render/GuiFluidRenderer.java
@@ -7,6 +7,7 @@ import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.Tesselator;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.resources.ResourceLocation;
@@ -14,6 +15,7 @@ import net.minecraft.util.Mth;
 import net.minecraft.world.inventory.InventoryMenu;
 import net.minecraftforge.client.extensions.common.IClientFluidTypeExtensions;
 import net.minecraftforge.fluids.FluidStack;
+import org.joml.Matrix4f;
 
 /* CREDIT to KingLemming and the Team CoFH for this class.
  * This class is made from copy-pasted code from the CofhCore
@@ -23,14 +25,14 @@ import net.minecraftforge.fluids.FluidStack;
  */
 public class GuiFluidRenderer {
 
-    public static void drawBackground(int xPos, int yPos, FluidStack fluidStack, int capacity, int width, int height) {
+    public static void drawBackground(GuiGraphics graphics, int xPos, int yPos, FluidStack fluidStack, int capacity, int width, int height) {
         int resourceHeight = height - 2;
         int resourceWidth = width - 2;
         int amount = getScaled(fluidStack, resourceHeight, capacity);
-        drawFluid(xPos + 1, yPos + 1 + (resourceHeight - amount), fluidStack, resourceWidth, amount);
+        drawFluid(graphics, xPos + 1, yPos + 1 + (resourceHeight - amount), fluidStack, resourceWidth, amount);
     }
 
-    public static void drawFluid(int x, int y, FluidStack fluid, int width, int height) {
+    public static void drawFluid(GuiGraphics graphics, int x, int y, FluidStack fluid, int width, int height) {
         if (!fluid.isEmpty()) {
             RenderSystem.enableBlend();
             RenderSystem.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
@@ -38,23 +40,23 @@ public class GuiFluidRenderer {
             RenderSystem.setShader(GameRenderer::getPositionTexShader);
             RenderSystem.setShaderTexture(0, new ResourceLocation("textures/atlas/blocks.png"));
             setShaderColorFromInt(color);
-            drawTiledTexture(x, y, getTexture(IClientFluidTypeExtensions.of(fluid.getFluid()).getStillTexture(fluid)), width, height);
+            drawTiledTexture(graphics, x, y, getTexture(IClientFluidTypeExtensions.of(fluid.getFluid()).getStillTexture(fluid)), width, height);
         }
     }
 
-    public static void drawTiledTexture(int x, int y, TextureAtlasSprite icon, int width, int height) {
+    public static void drawTiledTexture(GuiGraphics graphics, int x, int y, TextureAtlasSprite icon, int width, int height) {
         for(int i = 0; i < width; i += 16) {
             for(int j = 0; j < height; j += 16) {
                 int drawWidth = Math.min(width - i, 16);
                 int drawHeight = Math.min(height - j, 16);
-                drawScaledTexturedModalRectFromSprite(x + i, y + j, icon, drawWidth, drawHeight);
+                drawScaledTexturedModalRectFromSprite(graphics, x + i, y + j, icon, drawWidth, drawHeight);
             }
         }
 
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
     }
 
-    public static void drawScaledTexturedModalRectFromSprite(int x, int y, TextureAtlasSprite icon, int width, int height) {
+    public static void drawScaledTexturedModalRectFromSprite(GuiGraphics graphics, int x, int y, TextureAtlasSprite icon, int width, int height) {
         if (icon != null) {
             float minU = icon.getU0();
             float maxU = icon.getU1();
@@ -63,13 +65,14 @@ public class GuiFluidRenderer {
             float u = minU + (maxU - minU) * (float)width / 16.0F;
             float v = minV + (maxV - minV) * (float)height / 16.0F;
 
+            Matrix4f matrix = graphics.pose().last().pose();
             Tesselator tesselator = Tesselator.getInstance();
             BufferBuilder buffer = tesselator.getBuilder();
             buffer.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
-            buffer.vertex(x, y + height, 0.0).uv(minU, v).endVertex();
-            buffer.vertex(x + width, y + height, 0.0).uv(u, v).endVertex();
-            buffer.vertex(x + width, y, 0.0).uv(u, minV).endVertex();
-            buffer.vertex(x, y, 0.0).uv(minU, minV).endVertex();
+            buffer.vertex(matrix, x, y + height, 0.0F).uv(minU, v).endVertex();
+            buffer.vertex(matrix,x + width, y + height, 0.0F).uv(u, v).endVertex();
+            buffer.vertex(matrix,x + width, y, 0.0F).uv(u, minV).endVertex();
+            buffer.vertex(matrix, x, y, 0.0F).uv(minU, minV).endVertex();
             tesselator.end();
         }
     }
