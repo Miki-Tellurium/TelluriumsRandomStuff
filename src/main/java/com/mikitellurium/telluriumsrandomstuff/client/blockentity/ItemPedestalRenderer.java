a136b7693e80f777cd9ff150fa0b004c7691c6f5
Miki_Tellurium <michelegamer14@gmail.com>
2023-11-04 15:01:20 +0100

    Updated item name rendering

--- src/main/java/com/mikitellurium/telluriumsrandomstuff/client/blockentity/ItemPedestalRenderer.java
+++ src/main/java/com/mikitellurium/telluriumsrandomstuff/client/blockentity/ItemPedestalRenderer.java
@@ -3,6 +3,7 @@ package com.mikitellurium.telluriumsrandomstuff.client.blockentity;
 import com.mikitellurium.telluriumsrandomstuff.common.blockentity.ItemPedestalBlockEntity;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.renderer.MultiBufferSource;
@@ -12,8 +13,12 @@ import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.util.FormattedCharSequence;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
+
+import java.util.List;
 
 public class ItemPedestalRenderer implements BlockEntityRenderer<ItemPedestalBlockEntity> {
 
@@ -36,11 +41,14 @@ public class ItemPedestalRenderer implements BlockEntityRenderer<ItemPedestalBlo
             // Item
             float f = (partialTick + (float) itemPedestal.getRotTick()) / 20.0F + itemPedestal.getRotOffset();
             poseStack.pushPose();
+
             poseStack.translate(0.5F, 1.3F, 0.5F);
             poseStack.scale(itemScale, itemScale, itemScale);
             poseStack.mulPose(Axis.YP.rotation(f));
+
             itemRenderer.renderStatic(itemStack, ItemDisplayContext.FIXED, packedLight, packedOverlay, poseStack,
                     bufferSource, itemPedestal.getLevel(), 0);
+
             poseStack.popPose();
             // Name display
             BlockPos pos = itemPedestal.getBlockPos();
@@ -48,19 +56,35 @@ public class ItemPedestalRenderer implements BlockEntityRenderer<ItemPedestalBlo
                     Minecraft.getInstance().player.blockPosition().getCenter(), 6.0D) && itemStack.hasCustomHoverName();
             if (isInDisplayDistance || itemPedestal.alwaysDisplayName()) {
                 poseStack.pushPose();
+
                 Component text = itemStack.getHoverName();
                 int textColor = itemStack.getRarity().getStyleModifier().apply(text.getStyle()).getColor().getValue();
-                float xPos = (float) (-font.width(text) / 2);
                 float backgroundOpacity = Minecraft.getInstance().options.getBackgroundOpacity(0.25F);
                 int backgroundColor = (int) (backgroundOpacity * 255.0F) << 24;
+
                 poseStack.translate(0.5F, 1.9F, 0.5F);
                 poseStack.mulPose(entityRenderer.cameraOrientation());
                 poseStack.mulPose(Axis.ZP.rotationDegrees(180.0F));
                 poseStack.scale(0.02F, 0.02F, 0.02F);
-                font.drawInBatch(text, xPos, 0.0F, textColor, false,
-                        poseStack.last().pose(), bufferSource, Font.DisplayMode.NORMAL, backgroundColor, packedLight);
+
+                this.renderItemName(poseStack, bufferSource, text, textColor, backgroundColor, packedLight);
+
                 poseStack.popPose();
             }
+        }
+    }
+
+    private void renderItemName(PoseStack poseStack, MultiBufferSource bufferSource, Component component,
+                                int textColor, int backGroundColor, int packedLight) {
+        final int maxWidth = 70;
+        List<FormattedCharSequence> charSequenceList = font.split(component, maxWidth);
+
+        int yPos = (-charSequenceList.size() + 1) * 10;
+        for (FormattedCharSequence text : charSequenceList) {
+            float xPos = (float) (-font.width(text) / 2);
+            font.drawInBatch(text, xPos, yPos, textColor, false, poseStack.last().pose(), bufferSource,
+                    Font.DisplayMode.NORMAL, backGroundColor, packedLight);
+            yPos += 10;
         }
     }
 
