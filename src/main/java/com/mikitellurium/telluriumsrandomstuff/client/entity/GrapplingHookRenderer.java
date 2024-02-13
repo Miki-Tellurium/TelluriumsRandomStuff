4bca37f3f488dedf38e8701b8ab0f9794d79263f
Miki_Tellurium <michelegamer14@gmail.com>
2024-02-13 15:25:05 +0100

    Fixed grappling hook renderer

--- src/main/java/com/mikitellurium/telluriumsrandomstuff/client/entity/GrapplingHookRenderer.java
+++ src/main/java/com/mikitellurium/telluriumsrandomstuff/client/entity/GrapplingHookRenderer.java
@@ -43,9 +43,9 @@ public class GrapplingHookRenderer extends EntityRenderer<GrapplingHookEntity> {
         Player player = grapplingHook.getPlayerOwner();
         ItemStack itemstack = player.getMainHandItem();
         poseStack.pushPose();
+        poseStack.pushPose();
 
-        poseStack.mulPose(new Quaternionf()
-                .rotationXYZ(0, grapplingHook.getYRot() * ((float)Math.PI / 180), 0));
+        poseStack.mulPose(new Quaternionf().rotationXYZ(0, grapplingHook.getYRot() * ((float)Math.PI / 180), 0));
         poseStack.mulPose(Axis.XN.rotationDegrees(grapplingHook.getViewXRot(partialTicks)));
         poseStack.mulPose(Axis.XP.rotationDegrees(180.0F));
 
@@ -54,66 +54,63 @@ public class GrapplingHookRenderer extends EntityRenderer<GrapplingHookEntity> {
         this.model.renderToBuffer(poseStack, vertexconsumer, packedLight, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
         poseStack.popPose();
 
-//        int i = player.getMainArm() == HumanoidArm.RIGHT ? 1 : -1;
-//        if (!itemstack.is(ModItems.GRAPPLING_HOOK.get())) {
-//            i = -i;
-//        }
-//        float f = player.getAttackAnim(partialTicks);
-//        float f1 = Mth.sin(Mth.sqrt(f) * (float)Math.PI);
-//        float f2 = Mth.lerp(partialTicks, player.yBodyRotO, player.yBodyRot) * ((float)Math.PI / 180F);
-//        double d0 = Mth.sin(f2);
-//        double d1 = Mth.cos(f2);
-//        double d2 = (double)i * 0.35D;
-//        double x;
-//        double y;
-//        double z;
-//        float height;
-//        if (this.entityRenderDispatcher.options.getCameraType().isFirstPerson() && player == Minecraft.getInstance().player) {
-//            double d7 = 960.0D / (double) this.entityRenderDispatcher.options.fov().get();
-//            Vec3 vec3 = this.entityRenderDispatcher.camera.getNearPlane().getPointOnPlane((float)i * 0.525F, -0.1F);
-//            vec3 = vec3.scale(d7);
-//            vec3 = vec3.yRot(f1 * 0.5F);
-//            vec3 = vec3.xRot(-f1 * 0.7F);
-//            x = Mth.lerp(partialTicks, player.xo, player.getX()) + vec3.x;
-//            y = Mth.lerp(partialTicks, player.yo, player.getY()) + vec3.y;
-//            z = Mth.lerp(partialTicks, player.zo, player.getZ()) + vec3.z;
-//            height = player.getEyeHeight();
-//        } else {
-//            x = Mth.lerp(partialTicks, player.xo, player.getX()) - d1 * d2 - d0 * 0.6D;
-//            y = player.yo + (double)player.getEyeHeight() + (player.getY() - player.yo) * (double)partialTicks - 0.45D;
-//            z = Mth.lerp(partialTicks, player.zo, player.getZ()) - d0 * d2 + d1 * 0.6D;
-//            height = player.isCrouching() ? -0.1875F : 0.0F;
-//        }
-//
-//        double lerpX = Mth.lerp(partialTicks, grapplingHook.xo, grapplingHook.getX());
-//        double lerpY = Mth.lerp(partialTicks, grapplingHook.yo, grapplingHook.getY()) + 0.32D;
-//        double lerpZ = Mth.lerp(partialTicks, grapplingHook.zo, grapplingHook.getZ());
-//        float pX = (float)(x - lerpX);
-//        float pY = (float)(y - lerpY) + height;
-//        float pZ = (float)(z - lerpZ);
-//        VertexConsumer vertexConsumer = bufferSource.getBuffer(RenderType.lineStrip());
-//        PoseStack.Pose pose = poseStack.last();
-//
-//        final int segments = 16;
-//        for(int k = 0; k <= segments; ++k) {
-//            stringVertex(pX, pY, pZ, vertexConsumer, pose,
-//                    fraction(k, segments), fraction(k + 1, segments));
-//        }
-//
-//        poseStack.popPose();
+        int i = player.getMainArm() == HumanoidArm.RIGHT ? 1 : -1;
+        if (!itemstack.is(ModItems.GRAPPLING_HOOK.get())) {
+            i = -i;
+        }
+        float f = player.getAttackAnim(partialTicks);
+        float f1 = Mth.sin(Mth.sqrt(f) * (float)Math.PI);
+        float f2 = Mth.lerp(partialTicks, player.yBodyRotO, player.yBodyRot) * ((float)Math.PI / 180F);
+        double d0 = Mth.sin(f2);
+        double d1 = Mth.cos(f2);
+        double d2 = (double)i * 0.35D;
+        double x;
+        double y;
+        double z;
+        float height;
+        if (this.entityRenderDispatcher.options.getCameraType().isFirstPerson() && player == Minecraft.getInstance().player) {
+            double d7 = 960.0D / (double) this.entityRenderDispatcher.options.fov().get();
+            Vec3 vec3 = this.entityRenderDispatcher.camera.getNearPlane().getPointOnPlane((float)i * 0.525F, -0.1F);
+            vec3 = vec3.scale(d7);
+            vec3 = vec3.yRot(f1 * 0.5F);
+            vec3 = vec3.xRot(-f1 * 0.7F);
+            x = Mth.lerp(partialTicks, player.xo, player.getX()) + vec3.x;
+            y = Mth.lerp(partialTicks, player.yo, player.getY()) + vec3.y;
+            z = Mth.lerp(partialTicks, player.zo, player.getZ()) + vec3.z;
+            height = player.getEyeHeight();
+        } else {
+            x = Mth.lerp(partialTicks, player.xo, player.getX()) - d1 * d2 - d0 * 0.6D;
+            y = player.yo + (double)player.getEyeHeight() + (player.getY() - player.yo) * (double)partialTicks - 0.45D;
+            z = Mth.lerp(partialTicks, player.zo, player.getZ()) - d0 * d2 + d1 * 0.6D;
+            height = player.isCrouching() ? -0.1875F : 0.0F;
+        }
+
+        float angle = (float) Math.sin(player.getXRot() * (Math.PI / 180.0F));
+        float angleLerp = Mth.lerp(Math.abs(angle), 0.0F, 0.4F) * Math.signum(angle);
+
+        double lerpX = Mth.lerp(partialTicks, grapplingHook.xo, grapplingHook.getX());
+        double lerpY = Mth.lerp(partialTicks, grapplingHook.yo, grapplingHook.getY()) + 0.47D + angleLerp;
+        double lerpZ = Mth.lerp(partialTicks, grapplingHook.zo, grapplingHook.getZ());
+        float pX = (float)(x - lerpX);
+        float pY = (float)(y - lerpY) + height;
+        float pZ = (float)(z - lerpZ);
+        VertexConsumer vertexConsumer = bufferSource.getBuffer(RenderType.lineStrip());
+        PoseStack.Pose pose = poseStack.last();
+
+        final int segments = 16;
+        for(int k = 0; k <= segments; ++k) {
+            stringVertex(pX, pY, pZ, vertexConsumer, pose, fraction(k, segments), fraction(k + 1, segments));
+        }
+
+        poseStack.popPose();
         super.render(grapplingHook, yaw, partialTicks, poseStack, bufferSource, packedLight);
     }
 
-    private static float fraction(int pNumerator, int pDenominator) {
-        return (float)pNumerator / (float)pDenominator;
+    private static float fraction(int numerator, int denominator) {
+        return (float)numerator / (float)denominator;
     }
 
-    private static void vertex(VertexConsumer vertexConsumer, Matrix4f matrix4f, Matrix3f normal, int lightmapUV,
-                               float pX, int pY, int pU, int pV) {
-        vertexConsumer.vertex(matrix4f, pX - 0.5F, (float)pY - 0.5F, 0.0F).color(255, 255, 255, 255).uv((float)pU, (float)pV).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(lightmapUV).normal(normal, 0.0F, 1.0F, 0.0F).endVertex();
-    }
-
-    private static void stringVertex(float pX, float pY, float pZ, VertexConsumer pConsumer, PoseStack.Pose pPose,
+    private static void stringVertex(float pX, float pY, float pZ, VertexConsumer vertexConsumer, PoseStack.Pose pose,
                                      float segmentStart, float segmentEnd) {
         float f = pX * segmentStart;
         float f1 = pY * (segmentStart * segmentStart + segmentStart) * 0.5F + 0.25F;
@@ -125,7 +122,7 @@ public class GrapplingHookRenderer extends EntityRenderer<GrapplingHookEntity> {
         f3 /= f6;
         f4 /= f6;
         f5 /= f6;
-        pConsumer.vertex(pPose.pose(), f, f1, f2).color(0, 0, 0, 255).normal(pPose.normal(), f3, f4, f5).endVertex();
+        vertexConsumer.vertex(pose.pose(), f, f1, f2).color(0, 0, 0, 255).normal(pose.normal(), f3, f4, f5).endVertex();
     }
 
     @Override
