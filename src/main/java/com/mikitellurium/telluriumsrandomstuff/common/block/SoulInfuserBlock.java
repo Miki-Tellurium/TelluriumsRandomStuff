ee1df98cb4d64cd545d9affb2cc4e44d366e1450
Miki_Tellurium <michelegamer14@gmail.com>
2024-02-27 14:31:37 +0100

    Changed soul infuser texture

--- src/main/java/com/mikitellurium/telluriumsrandomstuff/common/block/SoulInfuserBlock.java
+++ src/main/java/com/mikitellurium/telluriumsrandomstuff/common/block/SoulInfuserBlock.java
@@ -67,23 +67,20 @@ public class SoulInfuserBlock extends AbstractFurnaceBlock {
     @Override
     public void animateTick(BlockState blockState, Level level, BlockPos pos, RandomSource random) {
         if (blockState.getValue(LIT)) {
-            double pX = pos.getX() + 0.5D;
-            double pY = pos.getY() + 0.7D;
-            double pZ = pos.getZ() + 0.5D;
+            double x = pos.getX() + 0.5D;
+            double y = pos.getY();
+            double z = pos.getZ() + 0.5D;
             if (random.nextDouble() < 0.1D) {
-                level.playLocalSound(pX, pY, pZ, SoundEvents.LAVA_POP, SoundSource.BLOCKS, 1.0F, 1.0F, false);
+                level.playLocalSound(x, y, z, SoundEvents.BLASTFURNACE_FIRE_CRACKLE, SoundSource.BLOCKS, 1.0F, 1.0F, false);
             }
 
-            if (random.nextInt(3) == 0) {
-                Direction direction = blockState.getValue(FACING);
-                Direction.Axis direction$axis = direction.getAxis();
-                double d0 = random.nextDouble() * 0.6D - 0.3D;
-                double d1 = direction$axis == Direction.Axis.X ? (double) direction.getStepX() * 0.52D : d0;
-                double d2 = random.nextDouble() * 3.0D / 16.0D;
-                double d3 = direction$axis == Direction.Axis.Z ? (double) direction.getStepZ() * 0.52D : d0;
-                level.addParticle(ParticleTypes.SMOKE, pX + d1, pY + d2, pZ + d3, 0.0D, -0.04D, 0.0D);
-                level.addParticle(ParticleTypes.SOUL_FIRE_FLAME, pX + d1, pY + d2, pZ + d3, 0.0D, -0.04D, 0.0D);
-            }
+            Direction direction = blockState.getValue(FACING);
+            Direction.Axis direction$axis = direction.getAxis();
+            double d = random.nextDouble() * 0.6D - 0.3D;
+            double xAxis = direction$axis == Direction.Axis.X ? direction.getStepX() * 0.52D : d;
+            double d1 = random.nextDouble() * 9.0D / 16.0D;
+            double yAxis = direction$axis == Direction.Axis.Z ? direction.getStepZ() * 0.52D : d;
+            level.addParticle(ParticleTypes.SMOKE, x + xAxis, y + d1, z + yAxis, 0.0D, 0.0D, 0.0D);
         }
     }
 
