44fc48ed2900ded51a1e728d307cf647e7b9b891
Miki_Tellurium <michelegamer14@gmail.com>
2024-02-27 14:48:33 +0100

    Decreased small soul fragment drop rate

--- src/main/java/com/mikitellurium/telluriumsrandomstuff/TelluriumsRandomStuffMod.java
+++ src/main/java/com/mikitellurium/telluriumsrandomstuff/TelluriumsRandomStuffMod.java
@@ -31,7 +31,7 @@ public class TelluriumsRandomStuffMod {
     // todo rei and mei compatibility
     // todo update changelog
     // todo opal armor trims
-    // todo change soul infuser texture
+
     public static final String MOD_ID = "telluriumsrandomstuff";
     public static final Logger LOGGER = LogUtils.getLogger();
 
