86deb913ed5123d93c89949794b69ece2d47d8bd
Miki_Tellurium <michelegamer14@gmail.com>
2024-05-04 17:34:15 +0200

    Added soul lava display

--- src/main/java/com/mikitellurium/telluriumsrandomstuff/integration/rei/display/SoulFurnaceSmeltingDisplay.java
+++ src/main/java/com/mikitellurium/telluriumsrandomstuff/integration/rei/display/SoulFurnaceSmeltingDisplay.java
@@ -2,8 +2,6 @@ package com.mikitellurium.telluriumsrandomstuff.integration.rei.display;
 
 import com.mikitellurium.telluriumsrandomstuff.common.recipe.SoulFurnaceSmeltingRecipe;
 import com.mikitellurium.telluriumsrandomstuff.integration.rei.category.SoulFurnaceSmeltingCategory;
-import com.mikitellurium.telluriumsrandomstuff.registry.ModFluids;
-import com.mikitellurium.telluriumsrandomstuff.registry.ModItems;
 import com.mikitellurium.telluriumsrandomstuff.util.RecipeHelper;
 import me.shedaniel.rei.api.common.category.CategoryIdentifier;
 import me.shedaniel.rei.api.common.display.DisplaySerializer;
@@ -13,12 +11,11 @@ import me.shedaniel.rei.api.common.util.EntryIngredients;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.SmeltingRecipe;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-public class SoulFurnaceSmeltingDisplay extends BasicDisplay {
+public class SoulFurnaceSmeltingDisplay extends SoulLavaDisplay {
 
     public SoulFurnaceSmeltingDisplay(SmeltingRecipe recipe) {
         this(RecipeHelper.convertSmelting(recipe));
@@ -33,16 +30,6 @@ public class SoulFurnaceSmeltingDisplay extends BasicDisplay {
     public SoulFurnaceSmeltingDisplay(List<EntryIngredient> inputs, List<EntryIngredient> outputs,
                                       Optional<ResourceLocation> location) {
         super(inputs, outputs, location);
-    }
-
-    @Override
-    public List<EntryIngredient> getInputEntries() {
-        List<EntryIngredient> inputs = new ArrayList<>(super.getInputEntries());
-        inputs.addAll(List.of(
-                EntryIngredients.of(ModItems.SOUL_LAVA_BUCKET.get()),
-                EntryIngredients.of(ModFluids.SOUL_LAVA_SOURCE.get())
-                ));
-        return inputs;
     }
 
     @Override
---
+++ src/main/java/com/mikitellurium/telluriumsrandomstuff/integration/rei/display/SoulLavaDisplay.java
@@ -0,0 +1,35 @@
+package com.mikitellurium.telluriumsrandomstuff.integration.rei.display;
+
+import com.mikitellurium.telluriumsrandomstuff.registry.ModFluids;
+import com.mikitellurium.telluriumsrandomstuff.registry.ModItems;
+import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.api.common.display.basic.BasicDisplay;
+import me.shedaniel.rei.api.common.entry.EntryIngredient;
+import me.shedaniel.rei.api.common.util.EntryIngredients;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public abstract class SoulLavaDisplay extends BasicDisplay {
+
+    public SoulLavaDisplay(List<EntryIngredient> inputs, List<EntryIngredient> outputs,
+                           Optional<ResourceLocation> location) {
+        super(inputs, outputs, location);
+    }
+
+    @Override
+    public List<EntryIngredient> getInputEntries() {
+        List<EntryIngredient> inputs = new ArrayList<>(super.getInputEntries());
+        inputs.addAll(List.of(
+                EntryIngredients.of(ModItems.SOUL_LAVA_BUCKET.get()),
+                EntryIngredients.of(ModFluids.SOUL_LAVA_SOURCE.get())
+                ));
+        return inputs;
+    }
+
+    @Override
+    public abstract CategoryIdentifier<?> getCategoryIdentifier();
+
+}
