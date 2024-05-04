b471886ae5865c61675224d9fb3dc41516dde1cb
Miki_Tellurium <michelegamer14@gmail.com>
2024-05-04 12:49:06 +0200

    Added input ingredients

--- src/main/java/com/mikitellurium/telluriumsrandomstuff/integration/rei/display/SoulFurnaceSmeltingDisplay.java
+++ src/main/java/com/mikitellurium/telluriumsrandomstuff/integration/rei/display/SoulFurnaceSmeltingDisplay.java
@@ -2,6 +2,9 @@ package com.mikitellurium.telluriumsrandomstuff.integration.rei.display;
 
 import com.mikitellurium.telluriumsrandomstuff.common.recipe.SoulFurnaceSmeltingRecipe;
 import com.mikitellurium.telluriumsrandomstuff.integration.rei.category.SoulFurnaceSmeltingCategory;
+import com.mikitellurium.telluriumsrandomstuff.registry.ModFluids;
+import com.mikitellurium.telluriumsrandomstuff.registry.ModItems;
+import com.mikitellurium.telluriumsrandomstuff.util.RecipeHelper;
 import me.shedaniel.rei.api.common.category.CategoryIdentifier;
 import me.shedaniel.rei.api.common.display.DisplaySerializer;
 import me.shedaniel.rei.api.common.display.basic.BasicDisplay;
@@ -10,6 +13,7 @@ import me.shedaniel.rei.api.common.util.EntryIngredients;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.SmeltingRecipe;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -17,9 +21,7 @@ import java.util.Optional;
 public class SoulFurnaceSmeltingDisplay extends BasicDisplay {
 
     public SoulFurnaceSmeltingDisplay(SmeltingRecipe recipe) {
-        this(EntryIngredients.ofIngredients(recipe.getIngredients()),
-                Collections.singletonList(EntryIngredients.of(recipe.getResultItem(BasicDisplay.registryAccess()))),
-                Optional.of(recipe.getId()));
+        this(RecipeHelper.convertSmelting(recipe));
     }
 
     public SoulFurnaceSmeltingDisplay(SoulFurnaceSmeltingRecipe recipe) {
@@ -31,6 +33,16 @@ public class SoulFurnaceSmeltingDisplay extends BasicDisplay {
     public SoulFurnaceSmeltingDisplay(List<EntryIngredient> inputs, List<EntryIngredient> outputs,
                                       Optional<ResourceLocation> location) {
         super(inputs, outputs, location);
+    }
+
+    @Override
+    public List<EntryIngredient> getInputEntries() {
+        List<EntryIngredient> inputs = new ArrayList<>(super.getInputEntries());
+        inputs.addAll(List.of(
+                EntryIngredients.of(ModItems.SOUL_LAVA_BUCKET.get()),
+                EntryIngredients.of(ModFluids.SOUL_LAVA_SOURCE.get())
+                ));
+        return inputs;
     }
 
     @Override
