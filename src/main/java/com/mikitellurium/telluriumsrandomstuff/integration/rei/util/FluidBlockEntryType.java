9342a4cffa17e8246592f5e73c273e81e4401eda
Miki_Tellurium <michelegamer14@gmail.com>
2024-05-11 13:00:37 +0200

    Optimized imports

--- src/main/java/com/mikitellurium/telluriumsrandomstuff/integration/rei/util/FluidBlockEntryType.java
+++ src/main/java/com/mikitellurium/telluriumsrandomstuff/integration/rei/util/FluidBlockEntryType.java
@@ -12,7 +12,6 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.material.Fluid;
-import net.minecraft.world.level.material.Fluids;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.Nullable;
