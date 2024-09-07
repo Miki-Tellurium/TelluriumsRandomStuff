cb3e0f20b45b38132c1094005bcaf5a5ca8aee6b
Miki_Tellurium <michelegamer14@gmail.com>
2024-09-07 16:59:30 +0200

    Removed todo

--- src/main/java/com/mikitellurium/telluriumsrandomstuff/common/capability/GrapplingHookCapabilityProvider.java
+++ src/main/java/com/mikitellurium/telluriumsrandomstuff/common/capability/GrapplingHookCapabilityProvider.java
@@ -12,7 +12,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class GrapplingHookCapabilityProvider implements ICapabilityProvider {
-    // todo add abstraction to providers
+
     public static Capability<GrapplingHookCapability> INSTANCE = CapabilityManager.get(new CapabilityToken<>() {});
 
     private GrapplingHookCapability grapplingHookCapability = null;
