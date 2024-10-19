c85f4a4cfd0e5845e1e216495ea0e3bce41f7310
Miki_Tellurium <michelegamer14@gmail.com>
2024-10-19 10:37:41 +0200

    Removed import

--- src/main/java/com/mikitellurium/telluriumsrandomstuff/datagen/providers/ModRecipeProvider.java
+++ src/main/java/com/mikitellurium/telluriumsrandomstuff/datagen/providers/ModRecipeProvider.java
@@ -12,14 +12,12 @@ import com.mikitellurium.telluriumsrandomstuff.util.RecipeHelper;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.*;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 
-import java.util.Map;
 import java.util.function.Consumer;
 
 public class ModRecipeProvider extends RecipeProvider {
